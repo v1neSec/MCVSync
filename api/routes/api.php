9a024720ce53d29853d0e4cffb87d7e5eec684ca
rshdgvn <rasheedgavinesponga@gmail.com>
2026-08-02 17:01:54 +0800

    chore: config sanctum session based auth

---
+++ api/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedUserController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+
+// Route::post('/login', LoginController::class);
+
+// Route::middleware('auth:sanctum')->group(function () {
+
+//     Route::get('/user', AuthenticatedUserController::class);
+
+//     Route::post('/logout', LogoutController::class);
+
+// });
