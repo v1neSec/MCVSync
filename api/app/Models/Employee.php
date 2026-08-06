ced9d6e9413c077acb878a5d67299d7dfc42e581
rshdgvn <rasheedgavinesponga@gmail.com>
2026-08-06 13:26:54 +0800

    feat: add position column to employees table

--- api/app/Models/Employee.php
+++ api/app/Models/Employee.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['user_id', 'branch_id'])]
+#[Fillable(['user_id', 'branch_id', 'position'])]
 class Employee extends Model
 {
     use HasFactory;
